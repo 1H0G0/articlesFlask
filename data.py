0a09ee0db00a8f29f6db0f8a156c27d7bb598187
1H0G0 <gagaravel@gmail.com>
2020-09-15 17:55:57 +0300

    Part 3/4

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One', 
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem expedita adipisci laudantium doloribus illo hic, perferendis nostrum, voluptatibus praesentium, quasi esse vero dolor quisquam minus ratione cum aliquid. Blanditiis, aliquam!',
+            'author': 'Garry RAV', 
+            'create_date': '01-09-2020'
+        }, 
+        {
+            'id': 2,
+            'title': 'Article Two', 
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem expedita adipisci laudantium doloribus illo hic, perferendis nostrum, voluptatibus praesentium, quasi esse vero dolor quisquam minus ratione cum aliquid. Blanditiis, aliquam!',
+            'author': 'Ihaga RAV', 
+            'create_date': '03-09-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three', 
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem expedita adipisci laudantium doloribus illo hic, perferendis nostrum, voluptatibus praesentium, quasi esse vero dolor quisquam minus ratione cum aliquid. Blanditiis, aliquam!',
+            'author': 'José RAV', 
+            'create_date': '03-09-2020'
+        }
+    ]
+    return articles
